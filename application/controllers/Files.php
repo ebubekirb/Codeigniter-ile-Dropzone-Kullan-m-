e38d3c37d5559aa9223027e8c8bf9a10414c29c2
Ebubekir Bingöloğlu <ebubekir@localhost.localdomain>
2018-12-26 01:18:06 +0300

    dosya upload islemi yapildi ve upload edilen dosyalarin veritabanina aktarimi gerceklestirildi.

--- application/controllers/Files.php
+++ application/controllers/Files.php
@@ -9,7 +9,30 @@ class Files extends CI_Controller{
 
 	public function upload(){
 
-		echo "test";
+		// $config["allowed_types"] = "*";	-> Tüm dosyalar...
+		$config["allowed_types"] 	= "jpg|gif|png";
+		$config["upload_path"] 		= "uploads/";
+
+		$this->load->library("upload", $config);
+
+		//do_upload() metodu true veya false olarak bir deger dondurur.(islem basarili veya basarisiz)
+		if ($this->upload->do_upload("file")) {
+		 	
+			$file_name = $this->upload->data("file_name");
+
+			$data = array(
+
+				"file_name" => $file_name,
+				"file_url" => base_url("uploads/$file_name")
+			);
+
+			$insert = $this->db->insert("files", $data);
+		 } 
+
+		 else{
+
+		 	echo "Başarısız";
+		 }
 	}
 }
 
